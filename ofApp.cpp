78e6890dd6c0149e5946fa79f9254c8c81e25989
NAKAUCHI Kiyoshi <junkiyoshi@gmail.com>
2022-08-18 20:16:51 +0900

    Add files via upload

---
+++ ofApp.cpp
@@ -0,0 +1,59 @@
+#include "ofApp.h"	
+
+//--------------------------------------------------------------
+void ofApp::setup() {
+
+	ofSetFrameRate(60);
+	ofSetWindowTitle("openFrameworks");
+
+	this->number_of_targets = 256;
+	this->shader.load("shader/shader.vert", "shader/shader.frag");
+}
+
+//--------------------------------------------------------------
+void ofApp::update() {
+
+	ofSeedRandom(39);
+
+	this->target_list.clear();
+	this->color_list.clear();
+
+	for (int i = 0; i < this->number_of_targets; i++) {
+
+		auto radius = ofRandom(200, 300);
+		auto deg = ofRandom(360) + ofGetFrameNum() * ofRandom(0.25, 0.8) * (ofRandom(1) < 0.5 ? 1 : -1);
+
+		auto target = glm::vec2(radius * cos(deg * DEG_TO_RAD), radius * sin(deg * DEG_TO_RAD));
+		this->target_list.push_back(target + ofGetWindowSize() * 0.5);
+
+		this->color_list.push_back(glm::vec3(
+			ofNoise(ofRandom(1000), ofGetFrameNum() * 0.015), 
+			ofNoise(ofRandom(1000), ofGetFrameNum() * 0.015),
+			ofNoise(ofRandom(1000), ofGetFrameNum() * 0.015)));
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::draw() {
+
+	ofFill();
+	this->shader.begin();
+	this->shader.setUniform1f("time", ofGetElapsedTimef());
+	this->shader.setUniform2f("resolution", ofGetWidth(), ofGetHeight());
+	this->shader.setUniform2fv("targets", &this->target_list[0].x, this->number_of_targets);
+	this->shader.setUniform3fv("colors", &this->color_list[0].x, this->number_of_targets);
+
+	ofDrawRectangle(0, 0, ofGetWidth(), ofGetHeight());
+
+	this->shader.end();
+}
+
+//--------------------------------------------------------------
+int main() {
+
+	ofGLWindowSettings settings;
+	settings.setGLVersion(3, 2);
+	settings.setSize(720, 720);
+	ofCreateWindow(settings);
+	ofRunApp(new ofApp());
+}
